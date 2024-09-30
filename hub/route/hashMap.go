a791c4774c24bf5fb948da72971228e9f55168ce
finddiff <yingchang.wei@gmail.com>
2024-09-30 14:59:28 +0800

    修改提示内容

--- hub/route/hashMap.go
+++ hub/route/hashMap.go
@@ -68,29 +68,11 @@ func getHashMap(w http.ResponseWriter, r *http.Request) {
 
 func getHashMaps(w http.ResponseWriter, r *http.Request) {
 	items := []HashMap{}
-	//for item := range tunnel.Cm.Iter() {
-	//	items = append(items, HashMap{
-	//		Key:   item.Key,
-	//		Value: fmt.Sprintf("%v", item.Val),
-	//	})
-	//}
-
-	//tunnel.Cm.ForEachFunc(func(key string, i *CC.Item) bool {
-	//	items = append(items, HashMap{
-	//		Key:   key,
-	//		Value: fmt.Sprintf("%v", i.Value()),
-	//	})
-	//	return true
-	//})
 
 	render.JSON(w, r, render.M{
 		"hashMap": items,
 	})
 }
-
-//func clearHashMap() {
-//	tunnel.Cm.Clear()
-//}
 
 func updateHashMap(w http.ResponseWriter, r *http.Request) {
 	req := HashMap{}
@@ -132,7 +114,6 @@ func updateHashMap(w http.ResponseWriter, r *http.Request) {
 		tunnel.UpdateRules(newRule)
 		tunnel.DeleteStrRule(db_key)
 	} else {
-
 		tunnel.UpdateRules(append([]C.Rule{newRule}, tunnel.Rules()...))
 		tunnel.CloseRuleMatchCon(newRule)
 		tunnel.AddStrRule(db_key, fmt.Sprintf("%s,%s,%s", req.Type, req.Key, req.Value))
@@ -140,62 +121,3 @@ func updateHashMap(w http.ResponseWriter, r *http.Request) {
 
 	render.NoContent(w, r)
 }
-
-//
-//func OrgUpdateHashMap(w http.ResponseWriter, r *http.Request) {
-//	req := HashMap{}
-//	//req :=
-//	log.Infoln("updateHashMap r.Body:%v", r.Body)
-//	if err := render.DecodeJSON(r.Body, &req); err != nil {
-//		render.Status(r, http.StatusBadRequest)
-//		render.JSON(w, r, ErrBadRequest)
-//		log.Infoln("updateHashMap err:%v", ErrBadRequest)
-//		return
-//	}
-//
-//	if strings.Contains(req.Key, ":") {
-//		req.Key = req.Key[:strings.Index(req.Key, ":")]
-//	}
-//
-//	//tunnel.Cm.Set(req.Key, req.Value)
-//	if req.Value == "DELETE" {
-//		var newRule = []C.Rule{}
-//		if net.ParseIP(req.Key) != nil {
-//			req.Key = req.Key + "/32"
-//			tunnel.DeleteIPRule(req.Key)
-//		} else {
-//			tunnel.DeleteDomainRule(req.Key)
-//		}
-//		for _, rule := range tunnel.Rules() {
-//			if rule.Payload() != req.Key {
-//				newRule = append(newRule, rule)
-//			} else {
-//				tunnel.CloseRuleMatchCon(rule)
-//			}
-//		}
-//		tunnel.UpdateRules(newRule)
-//	} else {
-//		var newRule C.Rule
-//		var err error
-//		newRule = nil
-//		err = nil
-//		if net.ParseIP(req.Key) != nil {
-//			newRule, err = R.NewIPCIDR(req.Key+"/32", req.Value, R.WithIPCIDRNoResolve(true))
-//			if err == nil {
-//				tunnel.UpdateRules(append([]C.Rule{newRule}, tunnel.Rules()...))
-//				tunnel.AddIPRule(req.Key+"/32", req.Value)
-//			}
-//		} else {
-//			newRule = R.NewDomainKeyword(req.Key, req.Value)
-//			tunnel.UpdateRules(append([]C.Rule{newRule}, tunnel.Rules()...))
-//			tunnel.AddDomainRule(req.Key, req.Value)
-//		}
-//		log.Debugln("CloseRuleMatchCon newRule:%v", newRule)
-//		if newRule != nil {
-//			tunnel.CloseRuleMatchCon(newRule)
-//		}
-//	}
-//	tunnel.Cm.Clear()
-//	log.Infoln("updateHashMap set %v:%v", req.Key, req.Value)
-//	render.NoContent(w, r)
-//}
--- tunnel/rulesSaveDB.go
+++ tunnel/rulesSaveDB.go
@@ -10,113 +10,7 @@ import (
 	"strings"
 )
 
-// const MapDomainRule = "map-domain-rule"
-// const MapIPRule = "map-domain-rule"
 const MapStringRule = "map-string-rule"
-
-//var (
-//	TakeSpaceValue = []byte{}
-//)
-
-//func AddDomainRule(key string, value string) {
-//	err := Persistence.RuleDB.Update(func(tx *nutsdb.Tx) error {
-//		//add new to maps
-//		log.Debugln("AddDomainRule add new to maps key:%v", key)
-//		err := tx.Put(MapDomainRule, []byte(key), []byte(value), 0)
-//		if err != nil {
-//			log.Errorln("tx.Put(MapDomainRule, []byte(key), []byte(value), 0) %v", err)
-//		}
-//		return nil
-//	})
-//	if err != nil {
-//		log.Errorln("AddDomainRule db.Update(func(tx *nutsdb.Tx) error  %v", err)
-//	}
-//}
-//
-//func DeleteDomainRule(key string) {
-//	err := Persistence.RuleDB.Update(func(tx *nutsdb.Tx) error {
-//		//add new to maps
-//		log.Debugln("AddDomainRule add new to maps key:%v", key)
-//		err := tx.Delete(MapDomainRule, []byte(key))
-//		if err != nil {
-//			log.Errorln("tx.Delete(MapDomainRule, []byte(key)) %v", err)
-//		}
-//		return nil
-//	})
-//	if err != nil {
-//		log.Errorln("AddDomainRule db.Update(func(tx *nutsdb.Tx) error  %v", err)
-//	}
-//}
-//
-//func LoadDomainRule() []C.Rule {
-//	rules = []C.Rule{}
-//	err := Persistence.RuleDB.View(func(tx *nutsdb.Tx) error {
-//		entries, _ := tx.GetAll(MapDomainRule)
-//		for _, entry := range entries {
-//			key := string(entry.Key)
-//			value := string(entry.Value)
-//			log.Infoln("LoadDomainRule add R.NewDomainKeyword(key:%v, value:%v)", key, value)
-//			rules = append(rules, R.NewDomainKeyword(key, value))
-//		}
-//		return nil
-//	})
-//	if err != nil {
-//		log.Errorln("db.Update(func(tx *nutsdb.Tx) error %v", err)
-//	}
-//	return rules
-//}
-//
-//func AddIPRule(key string, value string) {
-//	err := Persistence.RuleDB.Update(func(tx *nutsdb.Tx) error {
-//		//add new to maps
-//		log.Debugln("AddDomainRule add new to maps key:%v", key)
-//		err := tx.Put(MapIPRule, []byte(key), []byte(value), 0)
-//		if err != nil {
-//			log.Errorln("tx.Put(MapDomainRule, []byte(key), []byte(value), 0) %v", err)
-//		}
-//		return nil
-//	})
-//	if err != nil {
-//		log.Errorln("AddDomainRule db.Update(func(tx *nutsdb.Tx) error  %v", err)
-//	}
-//}
-//
-//func DeleteIPRule(key string) {
-//	err := Persistence.RuleDB.Update(func(tx *nutsdb.Tx) error {
-//		//add new to maps
-//		log.Debugln("AddDomainRule add new to maps key:%v", key)
-//		err := tx.Delete(MapIPRule, []byte(key))
-//		if err != nil {
-//			log.Errorln("tx.Delete(MapDomainRule, []byte(key)) %v", err)
-//		}
-//		return nil
-//	})
-//	if err != nil {
-//		log.Errorln("AddDomainRule db.Update(func(tx *nutsdb.Tx) error  %v", err)
-//	}
-//}
-//
-//func LoadIPRule() []C.Rule {
-//	rules = []C.Rule{}
-//	err := Persistence.RuleDB.View(func(tx *nutsdb.Tx) error {
-//		entries, _ := tx.GetAll(MapIPRule)
-//		for _, entry := range entries {
-//			key := string(entry.Key)
-//			value := string(entry.Value)
-//			log.Infoln("LoadIPRule add R.NewIPCIDR(key:%v, value:%v, R.WithIPCIDRNoResolve(true))", key, value)
-//			newRule, err := R.NewIPCIDR(key, value, R.WithIPCIDRNoResolve(true))
-//			if err != nil {
-//				continue
-//			}
-//			rules = append(rules, newRule)
-//		}
-//		return nil
-//	})
-//	if err != nil {
-//		log.Errorln("db.Update(func(tx *nutsdb.Tx) error %v", err)
-//	}
-//	return rules
-//}
 
 func TrimArr(arr []string) (r []string) {
 	for _, e := range arr {
@@ -136,7 +30,7 @@ func AddStrRule(key string, value string) {
 		return nil
 	})
 	if err != nil {
-		log.Errorln("AddDomainRule db.Update(func(tx *nutsdb.Tx) error  %v", err)
+		log.Errorln("AddStrRule db.Update(func(tx *nutsdb.Tx) error  %v", err)
 	}
 }
 
@@ -151,7 +45,7 @@ func DeleteStrRule(key string) {
 		return nil
 	})
 	if err != nil {
-		log.Errorln("AddDomainRule db.Update(func(tx *nutsdb.Tx) error  %v", err)
+		log.Errorln("DeleteStrRule db.Update(func(tx *nutsdb.Tx) error  %v", err)
 	}
 }
 
@@ -192,8 +86,6 @@ func LoadStrRule() []C.Rule {
 
 func LoadRule(rules []C.Rule) []C.Rule {
 	newRules := []C.Rule{}
-	//newRules = append(newRules, LoadDomainRule()...)
-	//newRules = append(newRules, LoadIPRule()...)
 	newRules = append(newRules, LoadStrRule()...)
 	newRules = append(newRules, rules...)
 	return newRules
